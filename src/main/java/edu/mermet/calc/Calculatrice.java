e50d966a521979c837600d647ba14190ca500d1d
Bruno Mermet <Bruno.Mermet@univ-lehavre.fr>
2018-11-24 01:57:48 +0100

    Version de base avec un pom à peu près configuré pour les rapports demandés. Cependant, après avoir "forké" le projet" pour créer le votre, il faudra penser à : 	- changer le groupId ; 	- changer le package de base des classes ; 	- changer la version pour maven (actuellement, c'est la 0.0) 	- changer l'URL du projet

---
+++ src/main/java/edu/mermet/calc/Calculatrice.java
@@ -0,0 +1,15 @@
+package edu.mermet.calc;
+
+/**
+ * Calculatrice RPN.
+ *
+ */
+public class Calculatrice {
+  public void entrer(String texteSaisi) {
+  }
+
+  public String toString() {
+    String retour = "1: 12\n";
+    return retour;
+  }
+}
