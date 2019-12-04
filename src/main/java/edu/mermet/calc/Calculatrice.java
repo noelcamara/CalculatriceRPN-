9118638c89b291e15245456654020ed16b4fe89e
Deborah Muzaneza <debokabkan@gmail.com>
2019-12-04 15:16:58 +0100

    ajout de fonction log et ln

--- src/main/java/edu/mermet/calc/Calculatrice.java
+++ src/main/java/edu/mermet/calc/Calculatrice.java
@@ -1,15 +1,53 @@
 package edu.mermet.calc;
 
+import java.util.Stack;
+
 /**
  * Calculatrice RPN.
  *
  */
 public class Calculatrice {
-  public void entrer(String texteSaisi) {
+
+  private Stack<Double> pile;
+
+  public Calculatrice(){
+    pile = new Stack<>();
   }
 
+  public void inverse(){
+    pile.add( 1/pile.pop() );
+  }
+
+  public void log(double x){
+    pile.add( Math.log(x)/Math.log(10) );
+  }
+   public void ln(double x){
+    pile.add( Math.log(x) );
+  }
+
+
+  public void entrer(String texteSaisi) {
+    if( texteSaisi.matches( "^[0-9][0-9.]*$" ) ){
+      pile.add( Double.parseDouble( texteSaisi ) );
+    }else{
+      switch( texteSaisi ) {
+        case "\\":
+          inverse();
+         case "log":
+          log();
+          case "ln": ln();
+
+      }
+    }
+  }
+
+  public String pile(){
+    return pile.size() + ": " + pile.peek();
+  }
+  
   public String toString() {
     String retour = "1: 12\n";
     return retour;
   }
 }
+
