3bfb40a63eebd15105f570732ce19618dcb19fa9
ae177527 <406-ae177527@users.noreply.www-apps.univ-lehavre.fr>
2019-12-04 15:44:43 +0100

    Replace Calculatrice.java

--- src/main/java/edu/mermet/calc/Calculatrice.java
+++ src/main/java/edu/mermet/calc/Calculatrice.java
@@ -1,62 +1,62 @@
 package edu.mermet.calc;
 
 import java.util.Stack;
-
 /**
  * Calculatrice RPN.
  *
  */
-public class Calculatrice {
+public class Calculatrice{
 
-  private Stack<Double> pile;
+	  private Stack<Double> pile;
 
-  public Calculatrice(){
-    pile = new Stack<>();
-  }
-
-  public void inverse(){
-    pile.add( 1/pile.pop() );
-  }
-
-  public void log(double x){
-    pile.add( Math.log(x)/Math.log(10) );
-  }
-   public void ln(double x){
-    pile.add( Math.log(x) );
-  }
+	  public Calculatrice(){
+	    pile = new Stack<>();
+	  }
 
 
-
+  
   public void entrer(String texteSaisi) {
-    if( texteSaisi.matches( "^[0-9][0-9.]*$" ) ){
-      pile.add( Double.parseDouble( texteSaisi ) );
-    }else{
-      switch( texteSaisi ) {
-        case "\\":
-          inverse();
-
-         case "log":
-          log(double x);
-          case "ln": ln(double x);
-
-      }
-    }
-      
-   }
-  
-
-  public String pile(){
-    return pile.size() + ": " + pile.peek();
-
   }
 
-  public String pile(){
-    return pile.size() + ": " + pile.peek();
+  
+  public void produit(){
+	  pile.add( pile.pop() * pile.pop());
+  }
+  public void division(){
+	  pile.add(pile.pop() / pile.pop());
+  }  
+  public void oppposit(){
+	  pile.add(-pile.pop());
+	  //pile.add(-pile.pop().doubleValue()); 
   }
   
-  public String toString() {
-    String retour = "1: 12\n";
-    return retour;
+  public void puissance(){
+	  pile.add(Math.pow(pile.pop(), 2));
+  }
+
+  public void cosinus(){
+	  pile.add(Math.cos(pile.pop()));
+  }
+  
+  public void tangente(){
+	  pile.add(Math.tan(pile.pop()));
+  }
+  public void exponetielle(String op1){
+	  pile.add(Math.exp(pile.pop()));
+  }
+  public void raz(){
+	  pile.clear();
+  }
+  
+  public static void main(String[] args) {
+	  Calculatrice c = new Calculatrice();
+	  c.pile.add(2.0);
+	  c.pile.add(3.0);
+	  c.produit();
+	  c.oppposit();
+	  for(Double i : c.pile) {
+	      System.out.println(i);
+	  }
   }
 }
 
