44527cd13b30ea3878bb8a5b7e04284b02b11784
aa172855 <376-aa172855@users.noreply.www-apps.univ-lehavre.fr>
2019-12-04 15:37:36 +0100

    Update Calculatrice.java - AjoutAditionSoustractionDivEuclidiennePuissanceModulo

--- src/main/java/edu/mermet/calc/Calculatrice.java
+++ src/main/java/edu/mermet/calc/Calculatrice.java
@@ -6,6 +6,66 @@ import java.util.Stack;
  * Calculatrice RPN.
  *
  */
+ 
+ public void addition()
+{
+	int res = 0;
+
+	res = pile.pop() + pile.pop();
+
+	pile.add(res);
+}
+
+public void soustraction()
+{
+	int res = 0;
+	double op1, op2;
+	
+	int op2 = pile.pop();
+	int op1 = pile.pop()
+
+	res = op1 - op2;
+
+	pile.add(res);
+}
+
+public void modulo()
+{
+	int res = 0;
+	
+	int op2 = pile.pop();
+	int op1 = pile.pop()
+
+	res = op1 % op2;
+
+	pile.add(res);
+}
+
+public void divisionEuclidienne()
+{
+	int res = 0;
+
+	int op2 = pile.pop();
+	int op1 = pile.pop()
+
+	res = op1 / op2;
+
+
+	pile.add(res);
+}
+public void puissance()
+{
+	int op2 = pile.pop();
+	int res,op1 = pile.pop()
+
+	for(int i=1; i <= (op2 -1); i++)
+	{
+		res *= op1;
+		
+	}
+	
+	pile.add(res);
+}
 public class Calculatrice {
 
   private Stack<Double> pile;
